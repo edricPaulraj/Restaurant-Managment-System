afb2c6567f002cf2feed4b60f418f61e4d078a18
Edric paulraj <edricgabrielpaulraj@gmail.com>
2026-09-17 22:19:34 +0100

    Updated Railway database connection

--- ExpressProject1/routes/db.js
+++ ExpressProject1/routes/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'iriguchi.proxy.rlwy.net',
-    port: 43945,
-    user: 'root',
-    password: 'CgjVNTPZygNVxiKQCuqYAzyqlSrGSZVU',
-    database: 'railway',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
 });
